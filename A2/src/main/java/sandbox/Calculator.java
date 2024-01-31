3f81868a0caf83860e32dc0e1d44834c8514b5d3
chvaldez10 <christianvaldez021@gmail.com>
2024-01-31 11:20:17 -0700

    added more tests

--- A2/src/main/java/sandbox/Calculator.java
+++ A2/src/main/java/sandbox/Calculator.java
@@ -9,4 +9,12 @@ public class Calculator {
 	public static int subtract(int a, int b) {
 		return a - b;
 	}
+	
+	public static int divide(int a, int b) {
+		if(b == 0) {
+			throw new ArithmeticException("Cannot divide by zero.");
+		}
+		
+		return a/b;
+	}
 }
--- A2/src/test/java/sandbox/CalculatorTest.java
+++ A2/src/test/java/sandbox/CalculatorTest.java
@@ -1,11 +1,13 @@
-
-
 package sandbox;
 
 // JUnit 5
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Test cases for Calculator class using JUnit 5
@@ -22,5 +24,32 @@ public class CalculatorTest {
 		void addingPositiveAndNegative() {
 			assertEquals(1, Calculator.add(3, -2));
 		}
+	}
+	
+	@Nested
+	class SubtractionTest{
+		@Test
+		void subtractingTwoPositiveNumbers() {
+			assertEquals(5, Calculator.subtract(7, 2));
+		}
+		
+		@Test
+		void SubtractingPositiveAndNegative() {
+			assertEquals(5, Calculator.subtract(3, -2));
+		}
+	}
+	
+	@Test
+	@Timeout(value=500, unit=TimeUnit.MILLISECONDS)
+	@Tag("addition")
+	void testSimpleAdd() {
+		assertEquals(5, Calculator.add(2, 3));
+	}
+	
+	@Test
+	void testDivideByZeroException() {
+		assertThrows(ArithmeticException.class, () -> {
+			Calculator.divide(10, 0);
+		});
 	}
 }
