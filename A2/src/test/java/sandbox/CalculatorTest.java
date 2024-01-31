ff6c5990b850344e38df54a7a54895837d73dd83
chvaldez10 <christianvaldez021@gmail.com>
2024-01-31 09:02:34 -0700

    added junit 5 dependency

---
+++ A2/src/test/java/sandbox/CalculatorTest.java
@@ -0,0 +1,19 @@
+//package sandbox;
+//
+//import static org.junit.Assert.*;
+//import org.junit.jupiter.Befor
+//
+//import org.junit.Test;
+//
+//public class CalculatorTest {
+//	private Calculator calculator;
+//	
+//	@BeforeEach
+//	
+//
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
