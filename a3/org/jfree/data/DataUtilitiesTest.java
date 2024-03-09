6e547c997e2b49b3a7ded3724a84aabacf4075cb
Christian Valdez <69766712+chvaldez10@users.noreply.github.com>
2024-03-09 14:14:16 -0700

    added test suite for calc column total

--- a3/org/jfree/data/DataUtilitiesTest.java
+++ a3/org/jfree/data/DataUtilitiesTest.java
@@ -17,8 +17,90 @@ public class DataUtilitiesTest {
     }
     
  // test cases for calculateColumnTotal(Values2D data, int column, int[] validRows) ------------------------
+	@Test(expected = IllegalArgumentException.class)
+    public void calculateColumnTotalWithNullData() {
+		final int[] validRows = {0, 1, 2};
+		DataUtilities.calculateRowTotal(null, 0, validRows);
+    }
+	
+	@Test(expected = NullPointerException.class)
+	public void calculateColumnTotalWithRowNull() {
+    	final int column = 1;
+    	final int[] validRows = null;
+    	
+        mockingContext.checking(new Expectations() {{
+ 			// Setup 3 rows
+ 			oneOf(values).getRowCount(); will(returnValue(3));
+ 			// Setup 3 columns
+ 			oneOf(values).getColumnCount(); will(returnValue(3));
+         }});
+        DataUtilities.calculateColumnTotal(values, column, validRows);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void calculateColumnTotalWithRowsEmpty() {
+    	final int column = 1;
+    	final int[] validRows = {};
+    	
+        mockingContext.checking(new Expectations() {{
+ 			// Setup 3 rows
+ 			oneOf(values).getRowCount(); will(returnValue(3));
+ 			// Setup 3 columns
+ 			oneOf(values).getColumnCount(); will(returnValue(3));
+ 			
+ 			// Mocking the behavior for 3 rows and a specific column
+            oneOf(values).getValue(0, column); will(returnValue(2.5));
+            oneOf(values).getValue(1, column); will(returnValue(3.0));
+            oneOf(values).getValue(2, column); will(returnValue(4.5));
+         }});
+        Double result = DataUtilities.calculateColumnTotal(values, column, validRows);
+        assertEquals("The column total should be 0.0", 0.0, result, 0.00001d);
+	}
+	
 	@Test
-    public void columnTotalWithRows3x3Test() {
+	public void calculateColumnTotalWithNumOfRowsGreaterThanColumn() {
+    	final int column = 1;
+    	final int[] validRows = {0, 1, 2, 3};
+    	
+        mockingContext.checking(new Expectations() {{
+ 			// Setup 3 rows
+ 			oneOf(values).getRowCount(); will(returnValue(3));
+ 			
+ 			// Setup 3 columns
+ 			oneOf(values).getColumnCount(); will(returnValue(3));
+             
+ 			// Mocking the behavior for 3 rows and a specific column
+             oneOf(values).getValue(0, column); will(returnValue(2.5));
+             oneOf(values).getValue(1, column); will(returnValue(3.0));
+             oneOf(values).getValue(2, column); will(returnValue(4.5));
+         }});
+        double result = DataUtilities.calculateColumnTotal(values, column, validRows);
+        assertEquals("The column total should be 10.0", 10.0, result, 0.00001d);
+	}
+
+	@Test
+	public void calculateTotalWithNumRowsWithNull() {
+    	final int column = 1;
+    	final int[] validRows = {0, 1, 2};
+    	
+        mockingContext.checking(new Expectations() {{
+ 			// Setup 3 rows
+ 			oneOf(values).getRowCount(); will(returnValue(3));
+ 			
+ 			// Setup 3 columns
+ 			oneOf(values).getColumnCount(); will(returnValue(3));
+             
+ 			// Mocking the behavior for 3 rows and a specific column
+             oneOf(values).getValue(0, column); will(returnValue(2.5));
+             oneOf(values).getValue(1, column); will(returnValue(null));
+             oneOf(values).getValue(2, column); will(returnValue(4.5));
+         }});
+        double result = DataUtilities.calculateColumnTotal(values, column, validRows);
+        assertEquals("The column total should be 7.0", 7.0, result, 0.00001d);
+	}
+	
+    @Test
+    public void calculateColumnTotalWithRowsValidInputs() {
     	final int column = 1;
     	final int[] validRows = {0, 1, 2};
     	
